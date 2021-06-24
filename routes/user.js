90d34765b41683500f19a611c529f05037bb2101
viettu2k <doanviettu2k@gmail.com>
2021-06-24 15:11:28 +0700

    add delete user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {
-  userById,
-  allUsers,
-  getUser,
-  updateUser,
+    userById,
+    allUsers,
+    getUser,
+    updateUser,
+    deleteUser
 } = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
 router.put("/user/:userId", requireSignin, updateUser);
+router.delete("/user/:userId", requireSignin, deleteUser);
 
 // any route conatining: userId, our app will first excute userById()
 router.param("userId", userById);
