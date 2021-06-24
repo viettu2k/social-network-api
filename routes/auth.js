2cb4cb1b5225dbad8e7a70bcf1f2ebea574b377e
viettu2k <doanviettu2k@gmail.com>
2021-06-24 10:22:45 +0700

    find user by id and add to req object

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const { signup, signin, signout } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 const { userSignupValidator } = require("../validator");
 
 const router = express.Router();
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
-// sign out 
 router.get("/signout", signout);
+
+// any route conatining: userId, our app will first excute userById()
+router.param("userId", userById);
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { getPost, createPost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
 
 const router = express.Router();
 
 router.get("/", requireSignin, getPost);
 router.post("/post", createPostValidator, createPost);
+
+// any route conatining: userId, our app will first excute userById()
+router.param("userId", userById);
 
 module.exports = router;
