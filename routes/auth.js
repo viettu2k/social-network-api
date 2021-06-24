8d558268c397c0a57b3ba6cba1e43fc264f36d10
viettu2k <doanviettu2k@gmail.com>
2021-06-24 09:29:26 +0700

    add sign out

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/auth");
+const { signup, signin, signout } = require("../controllers/auth");
 const { userSignupValidator } = require("../validator");
 
 const router = express.Router();
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
+// sign out 
+router.get("/signout", signout);
 
 module.exports = router;
