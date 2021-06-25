a5a54f81591a768bcdd163d075c9a6460378814d
viettu2k <doanviettu2k@gmail.com>
2021-06-25 14:53:32 +0700

    add cors

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 const fs = require("fs");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
 //db connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB Connected"));
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"));
 
 mongoose.connection.on("error", (err) => {
-  console.log(`DB connection error: ${err.message}`);
+    console.log(`DB connection error: ${err.message}`);
 });
 
 // bring in routes
@@ -28,15 +29,15 @@ const userRoutes = require("./routes/user");
 
 // apiDocs
 app.get("/", (req, res) => {
-  fs.readFile("docs/apiDocs.json", (err, data) => {
-    if (err) {
-      res.status(400).json({
-        error: err,
-      });
-    }
-    const docs = JSON.parse(data);
-    res.json(docs);
-  });
+    fs.readFile("docs/apiDocs.json", (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err,
+            });
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
 });
 
 // middleware
@@ -44,17 +45,18 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
 app.use("/", userRoutes);
-app.use(function (err, req, res, next) {
-  if (err.name === "UnauthorizedError") {
-    res.status(401).json({ error: "Unauthorized!" });
-  }
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({ error: "Unauthorized!" });
+    }
 });
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`App is running on port: ${port}`);
+    console.log(`App is running on port: ${port}`);
 });
