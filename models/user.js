273a06a47a6186258ec65d44daf4a96a225ee3b9
viettu2k <doanviettu2k@gmail.com>
2021-07-01 10:15:15 +0700

    add update user with photo

--- models/user.js
+++ models/user.js
@@ -3,60 +3,64 @@ const { v1: uuidv1 } = require("uuid");
 const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  hashed_password: {
-    type: String,
-    required: true,
-  },
-  salt: String,
-  created: {
-    type: Date,
-    default: Date.now(),
-  },
-  updated: Date,
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now(),
+    },
+    updated: Date,
+    photo: {
+        data: Buffer,
+        contentType: String,
+    },
 });
 
 // virtual field
 userSchema
-  .virtual("password")
-  .set(function (password) {
-    // create temporary variable called _password
-    this._password = password;
-    // generate a timestamp
-    this.salt = uuidv1();
-    // encryptPassword()
-    this.hashed_password = this.encryptPassword(password);
-  })
-  .get(function () {
-    return this._password;
-  });
+    .virtual("password")
+    .set(function(password) {
+        // create temporary variable called _password
+        this._password = password;
+        // generate a timestamp
+        this.salt = uuidv1();
+        // encryptPassword()
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
 
 // methods
 userSchema.methods = {
-  authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password;
-  },
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
 
-  encryptPassword: function (password) {
-    if (!password) return "";
-    try {
-      return crypto
-        .createHmac("sha1", this.salt)
-        .update(password)
-        .digest("hex");
-    } catch (err) {
-      return "";
-    }
-  },
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    },
 };
 
 module.exports = mongoose.model("User", userSchema);
