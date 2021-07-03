662adccfb996b25a7e1ac342c1107315ae73ec5b
viettu2k <doanviettu2k@gmail.com>
2021-07-03 10:59:12 +0700

    add users follow and unfollow

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,17 @@ const {
     updateUser,
     deleteUser,
     userPhoto,
+    addFollowing,
+    addFollower,
+    removeFollowing,
+    removeFollower,
 } = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 
 const router = express.Router();
+
+router.put("user/follow", requireSignin, addFollowing, addFollower);
+router.put("user/unfollow", requireSignin, removeFollowing, removeFollower);
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
