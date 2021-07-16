60d79fd620cf62d6b8cc00af118cc1f93c1b27c5
viettu2k <doanviettu2k@gmail.com>
2021-07-16 13:23:22 +0700

    fix

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
     }
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
